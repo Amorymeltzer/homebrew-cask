c88db6018d4674df279231acd74a2ae299396acd
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-01 16:51:29 -0800

    Fix download links for Onyx on Mountain Lion and Mavericks

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -7,9 +7,9 @@ cask :v1 => 'onyx' do
   elsif MacOS.version == :lion
     url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
   elsif MacOS.version == :mountain_lion
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+    url 'http://www.titanium.free.fr/download/108/OnyX.dmg'
   elsif MacOS.version == :mavericks
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/109/OnyX.dmg'
+    url 'http://www.titanium.free.fr/download/109/OnyX.dmg'
   else
     url 'http://joel.barriere.pagesperso-orange.fr/dl/1010/OnyX.dmg'
   end
